b075549426640e281b59771f1f1d78e4f0b53945
Darlon Gomes <gomesdarlon@icloud.com>
2022-09-17 12:37:54 -0300

    test: route trouble on deploy

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-
-export default function HomePage (){
-    return(
-        <>
-        OLÁ
-        </>
-    )
+export default function HomePage() {
+  return (
+    <>
+      OLÁ
+      <Link to="/sign-in"> Sign In </Link>
+      <Link to="/sign-up"> Sign Up </Link>
+    </>
+  );
 }
